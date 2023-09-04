b562e51cee830bdc00bfa850399ca9e0dd4bf224
hahieupac <hahieupac@gmail.com>
2023-09-04 21:21:45 +0700

    09:21 030923

---
+++ src/containers/System/Admin/UserRedux.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+class UserRedux extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+    }
+
+
+    render() {
+        return (
+            <div className='user-redux-container'>
+                <div className="title" >
+                    user redux user "Hieu"
+                </div>
+                <div className='user-redux-body'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-3'>
+                                <label>Email</label>
+                                <input className='form-control' type='email'></input>
+                            </div>
+                            <div className='col-3'>
+                                <label>Password</label>
+                                <input className='form-control' type='password'></input>
+                            </div>
+                            <div className='col-3'>
+                                <label>First name</label>
+                                <input className='form-control' type='text'></input>
+                            </div>
+                            <div className='col-3'>
+                                <label>Last name</label>
+                                <input className='form-control' type='text'></input>
+                            </div>
+                            <div className='col-3'>
+                                <label>Phone number</label>
+                                <input className='form-control' type='text'></input>
+                            </div>
+                            <div className='col-9'>
+                                <label>Address</label>
+                                <input className='form-control' type='text'></input>
+                            </div>
+                            <div className='col-3'>
+                                <label>Gender</label>
+                                <input className='form-control' type='text'></input>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserRedux);
