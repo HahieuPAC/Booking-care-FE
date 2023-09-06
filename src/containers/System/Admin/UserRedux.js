9b0a15d7373840d76a99e8cda9f456059c585a26
HahieuPAC <hahieupac@gmail.com>
2023-09-06 09:58:09 +0700

    0953 060923

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -23,33 +23,58 @@ class UserRedux extends Component {
                 <div className='user-redux-body'>
                     <div className='container'>
                         <div className='row'>
+                            <div className='col-12  my-3'><FormattedMessage id="manage-user.add"/></div>
                             <div className='col-3'>
-                                <label>Email</label>
+                                <label><FormattedMessage id="manage-user.email"/></label>
                                 <input className='form-control' type='email'></input>
                             </div>
                             <div className='col-3'>
-                                <label>Password</label>
+                                <label><FormattedMessage id="manage-user.password"/></label>
                                 <input className='form-control' type='password'></input>
                             </div>
                             <div className='col-3'>
-                                <label>First name</label>
+                                <label><FormattedMessage id="manage-user.first-name"/></label>
                                 <input className='form-control' type='text'></input>
                             </div>
                             <div className='col-3'>
-                                <label>Last name</label>
+                                <label><FormattedMessage id="manage-user.last-name"/></label>
                                 <input className='form-control' type='text'></input>
                             </div>
                             <div className='col-3'>
-                                <label>Phone number</label>
+                                <label><FormattedMessage id="manage-user.phone-number"/></label>
                                 <input className='form-control' type='text'></input>
                             </div>
                             <div className='col-9'>
-                                <label>Address</label>
+                                <label><FormattedMessage id="manage-user.address"/></label>
                                 <input className='form-control' type='text'></input>
                             </div>
                             <div className='col-3'>
-                                <label>Gender</label>
+                                <label><FormattedMessage id="manage-user.gender"/></label>
+                                <select className="form-control">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                            </div>
+                            <div className='col-3'>
+                                <label><FormattedMessage id="manage-user.position"/></label>
+                                <select className="form-control">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                            </div>
+                            <div className='col-3'>
+                                <label><FormattedMessage id="manage-user.role"/></label>
+                                <select className="form-control">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                            </div>
+                            <div className='col-3'>
+                                <label><FormattedMessage id="manage-user.image"/></label>
                                 <input className='form-control' type='text'></input>
+                            </div>
+                            <div className='col-12 mt-3'>
+                                <button className='btn btn-primary'><FormattedMessage id="manage-user.save"/></button>
                             </div>
                         </div>
                     </div>
