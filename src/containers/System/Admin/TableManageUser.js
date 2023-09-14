2bdfe2c427f0d02a8e895525bed88a5aaa4c351b
hahieupac <hahieupac@gmail.com>
2023-09-14 22:20:18 +0700

    22h20 140923

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -7,6 +7,23 @@ import ModalUser from '../../System/ModalUser';
 import ModalEditUser from '../../System/ModalEditUser';
 import { emitter } from '../../../utils/emitter';
 import * as actions from "../../../store/actions";
+
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+// import style manually
+import 'react-markdown-editor-lite/lib/index.css';
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+    console.log('handleEditorChange', html, text);
+}
+
 class TableManageUser extends Component {
 
     constructor(props) {
@@ -40,39 +57,41 @@ class TableManageUser extends Component {
     render() {
         let arrUsers = this.state.userRedux;
         return (
-                    <table id = "TableManageUser">
-                        <tbody>
-                            <tr>
-                                <th>Email</th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Address</th>
-                                <th>Actions</th>
-                            </tr>
-                            {arrUsers && arrUsers.length > 0 && 
-                            arrUsers.map((item, index) => {
-                                return(
-                                    <tr key = {index}>
-                                        <td>{item.email}</td>
-                                        <td>{item.firstName}</td>
-                                        <td>{item.lastName}</td>
-                                        <td>{item.address}</td>
-                                        <td>
-                                            <button className='btn-edit'><i class="fas fa-edit"
-                                            onClick={()=>{this.handleEditUser(item)}}
-                                            ></i></button>
-                                            <button className='btn-delete'
-                                            onClick={()=> this.handleDeleteUser(item)}
-                                            ><i class="fas fa-trash-alt"></i></button>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
+            <>
+                <table id = "TableManageUser">
+                    <tbody>
+                        <tr>
+                            <th>Email</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        {arrUsers && arrUsers.length > 0 && 
+                        arrUsers.map((item, index) => {
+                            return(
+                                <tr key = {index}>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td>
+                                        <button className='btn-edit'><i class="fas fa-edit"
+                                        onClick={()=>{this.handleEditUser(item)}}
+                                        ></i></button>
+                                        <button className='btn-delete'
+                                        onClick={()=> this.handleDeleteUser(item)}
+                                        ><i class="fas fa-trash-alt"></i></button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+                <MdEditor style={{ height: '500px' }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
+            </>
         );
     }
-
 }
 
 const mapStateToProps = state => {
